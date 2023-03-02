9ebebbda0d408dfcfe3919a9c40bf77bb5e02a08
ihjaz24 <ihjazmohd19@gmail.com>
2023-03-02 19:17:12 +0530

    dtgfdhf

--- src/index.js
+++ src/index.js
@@ -15,6 +15,6 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log dghfchfvjhfvresults (for example: reportWebVitals(console.log))
+// to log dghfchfvjhfytfhfhyfhyfvresults (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
